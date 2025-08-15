0e7e002db6a73fbb4adaf5eae0ca9e2e08a288e9
Heli Eronen <herojonen@gmail.com>
2025-08-15 16:25:00 +0300

    Exercise 12.7

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -2,8 +2,15 @@ const mongoose = require('mongoose')
 const Todo = require('./models/Todo')
 const { MONGO_URL } = require('../util/config')
 
-if (MONGO_URL && !mongoose.connection.readyState) mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+console.log('Connecting to MongoDB...')
+console.log(MONGO_URL)
 
+if (MONGO_URL && !mongoose.connection.readyState) {
+  mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  console.log('Connected to MongoDB')
+} else {
+  console.error('MongoDB connection URL is not defined or already connected')
+}
 
 module.exports = {
   Todo

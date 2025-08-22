01edd7ffb0cb9197fcfecec06ecb666b6a306520
Heli Eronen <herojonen@gmail.com>
2025-08-22 15:51:38 +0300

    Exercise 12.10

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -7,12 +7,20 @@ const configs = require('../util/config')
 let visits = 0
 
 /* GET index data. */
-router.get('/', async (req, res) => {
+router.get('/', async (_req, res) => {
   visits++
 
   res.send({
     ...configs,
     visits
+  });
+});
+
+router.get('/statistics', async (_req, res) => {
+  const addedTodos = await redis.getAsync('added_todos')
+
+  res.json({
+    added_todos: Number(addedTodos) ?? 0,
   });
 });
 
